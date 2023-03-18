95df6d8c48ea4a7cb099963c90be07c10f8ee075
Eugene Stotozhuk <deus.lo.vult@mail.ru>
2023-03-19 01:41:18 +0500

    Исправил все замечания

--- src/ru/croc/task3/Main.java
+++ src/ru/croc/task3/Main.java
@@ -1,8 +1,7 @@
 package ru.croc.task3;
 
-import java.util.Scanner;
-
 public class Main {
+
     public static void main(String[] args) {
 
         if (args.length == 0) {
@@ -15,14 +14,18 @@ public class Main {
         int minIndex = minMaxIndexes[0];
         int maxIndex = minMaxIndexes[1];
 
-        vector = swapIndexes(vector,0,minIndex);
-        vector = swapIndexes(vector, vector.length-1 ,maxIndex);
+        vector = swapElements(vector, 0, minIndex);
+        if (maxIndex == 0) {
+            maxIndex = minIndex; // после первого swap'a максимальное число переехало по адресу minIndex
+        }
+        vector = swapElements(vector, vector.length - 1, maxIndex);
 
         printOutVector(vector);
     }
 
     public static int[] findMinMaxIndexes(int[] vector) {
-        int minIndex = 0, maxIndex = 0;
+        int minIndex = 0;
+        int maxIndex = 0;
         for (int i = 0; i < vector.length; i++) {
             if (vector[i] < vector[minIndex]) {
                 minIndex = i;
@@ -33,13 +36,15 @@ public class Main {
         }
         return new int[] {minIndex, maxIndex};
     }
+
     public static void printOutVector(int[] vector) {
         for (int i = 0; i < vector.length; i++) {
             System.out.print(vector[i] + " ");
         }
     }
-    public static int[] swapIndexes(int[] vector, int index1, int index2) {
-        int[] vecClone = vector.clone(); // по идее тут это только мешает, но изменять объект передаваемый.... странно было бы
+
+    public static int[] swapElements(int[] vector, int index1, int index2) {
+        int[] vecClone = vector.clone();
         int temp = vecClone[index1];
         vecClone[index1] = vecClone[index2];
         vecClone[index2] = temp;
@@ -49,8 +54,7 @@ public class Main {
     public static int[] readVector(String[] args) {
         int[] vector = new int[args.length];
         for (int i = 0; i < args.length; i++) {
-            vector[i] =
-                    new Scanner(args[i]+' ').nextInt();
+            vector[i] = Integer.parseInt(args[i]);
         }
         return vector;
     }
